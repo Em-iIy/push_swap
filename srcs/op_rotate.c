71903c2811cf62e863e98aea259e36518b8cae0d
Glenn Winnink <gwinnink@student.codam.nl>
2022-03-03 12:59:52 +0100

    renamed operations files

--- srcs/op_rotate.c
+++ srcs/op_rotate.c
@@ -1,35 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push.c                                             :+:      :+:    :+:   */
+/*   op_rotate.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/03 11:22:41 by gwinnink          #+#    #+#             */
-/*   Updated: 2022/03/03 12:52:59 by gwinnink         ###   ########.fr       */
+/*   Created: 2022/03/03 11:23:01 by gwinnink          #+#    #+#             */
+/*   Updated: 2022/03/03 12:57:21 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-void	push_b(t_stack *stack)
-{
-	t_node	*temp;
-
-	if (!stack->stack_a)
-		return ;
-	temp = stack->stack_a->next;
-	node_add_front(&stack->stack_b, stack->stack_a);
-	stack->stack_a = temp;
-}
-
-void	push_a(t_stack *stack)
-{
-	t_node	*temp;
-
-	if (!stack->stack_b)
-		return ;
-	temp = stack->stack_b->next;
-	node_add_front(&stack->stack_a, stack->stack_b);
-	stack->stack_b = temp;
-}
