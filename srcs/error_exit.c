99d178b21511d2cb8e909bec841293a7dd94a02d
Glenn Winnink <gwinnink@student.codam.nl>
2022-03-09 15:46:01 +0100

    removed error info messages

--- srcs/error_exit.c
+++ srcs/error_exit.c
@@ -6,24 +6,16 @@
 /*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 15:57:40 by gwinnink          #+#    #+#             */
-/*   Updated: 2022/03/08 16:34:24 by gwinnink         ###   ########.fr       */
+/*   Updated: 2022/03/09 15:38:09 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include "push_swap.h"
+#include "libft.h"
 #include <stdlib.h>
 
 void	error_exit(int code)
 {
 	ft_putstr_fd("Error\n", 2);
-	if (code == MALLOC_ERROR)
-		ft_putstr_fd("Ran out of memory", 2);
-	else if (code == TOO_FEW_ARGUMENTS)
-		ft_putstr_fd("Too few arguments", 2);
-	else if (code == INVALID_ARGUMENT)
-		ft_putstr_fd("Please input numbers only", 2);
-	else if (code == DUPLICATE_ARGUMENT)
-		ft_putstr_fd("Duplicate numbers", 2);
 	exit(code);
 }
